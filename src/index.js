1e61e5d195e82b6e32d50384809a3d98ec749ea8
Harry Wolff <hswolff@gmail.com>
2018-06-08 21:23:57 -0400

    feat: Add library

--- src/index.js
+++ src/index.js
@@ -1,9 +1,76 @@
-import React, {Component} from 'react'
+import React from 'react';
+import flatten from 'lodash.flatten';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+const lifecycleWhitelist = {
+  instance: [
+    'componentWillMount',
+    'UNSAFE_componentWillMount',
+    'componentDidMount',
+    'componentWillReceiveProps',
+    'UNSAFE_componentWillReceiveProps',
+    'shouldComponentUpdate',
+    'componentWillUpdate',
+    'UNSAFE_componentWillUpdate',
+    'getSnapshotBeforeUpdate',
+    'componentDidUpdate',
+    'componentWillUnmount',
+    'componentDidCatch',
+  ],
+  static: ['getDerivedStateFromProps'],
+  other: ['getInitialState'],
+};
+
+const allReactFunctions = flatten(Object.values(lifecycleWhitelist));
+
+function defaultMapStateToProps(state) {
+  return { state };
+}
+
+export default function withLifecyles(lifecycles = {}) {
+  const mapStateToProps = lifecycles.mapStateToProps || defaultMapStateToProps;
+
+  const nonReactFunctions = Object.keys(lifecycles).filter(
+    method => allReactFunctions.includes(method) === false
+  );
+
+  return function lifecycleCreator(Component) {
+    class LifecycleComponent extends React.Component {
+      constructor(props) {
+        super(props);
+
+        this.state = lifecycles.getInitialState
+          ? lifecycles.getInitialState(props)
+          : {};
+
+        this.boundSetState = this.setState.bind(this);
+      }
+
+      render() {
+        const componentProps = mapStateToProps(this.state);
+
+        nonReactFunctions.forEach(
+          fnName =>
+            (componentProps[fnName] = event =>
+              lifecycles[fnName]({
+                event,
+                state: this.state,
+                props: this.props,
+                setState: this.boundSetState,
+              }))
+        );
+
+        return <Component {...componentProps} />;
+      }
+    }
+
+    lifecycleWhitelist.instance.forEach(methodName => {
+      const methodValue = lifecycles[methodName];
+      if (!methodValue) {
+        return;
+      }
+      LifecycleComponent.prototype[methodName] = methodValue;
+    });
+
+    return LifecycleComponent;
+  };
 }
